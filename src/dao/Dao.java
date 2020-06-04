273bf645917e9899d77565abbb54071695020c55
fdz23 <ferdavid_9@hotmail.com>
2020-06-03 22:58:25 -0300

    Test getByID Dao

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -222,9 +222,9 @@ public abstract class Dao<T> implements IDao<T> {
 
         T item = criaItem(rs);
 
-        if (item == null) {
-            throw new Exception("Erro ao encontrar um item " + item.getClass().getName());
-        }
+        /*if (item == null) {
+            throw new Exception("Erro ao encontrar um item por id");
+        }*/
 
         return item;
 
