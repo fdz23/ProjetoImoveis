962c0b746ed86c9879ecc5cc25e95b4dbe472dd2
fdz23 <ferdavid_9@hotmail.com>
2020-06-06 17:56:36 -0300

    Adicionado Usuario

---
+++ src/controller/UsuarioController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.UsuarioDao;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
+import model.Usuario;
+
+/**
+ *
+ * @author fdz
+ */
+public class UsuarioController extends Controller<Usuario> {
+
+    
+    public UsuarioController() throws ClassNotFoundException, SQLException {
+        
+        dao = new UsuarioDao();
+        
+    }
+    
+    public DefaultTableModel populaJTable(DefaultTableModel model, int campo) throws Exception {
+        
+        return null;
+        
+    }
+}
